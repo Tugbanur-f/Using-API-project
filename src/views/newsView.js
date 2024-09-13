a3b7a922f8fac000ebd6ce6752c97e6ca550759a
Tugbanur <tnur.yurtsever@gmail.com>
2024-09-13 22:31:54 +0200

    image added

--- src/views/newsView.js
+++ src/views/newsView.js
@@ -9,12 +9,36 @@ export const createNewsContainer = () => {
 export const createNewsItem = (article) => {
     const newsItem = document.createElement('div');
     newsItem.classList.add('news-item');
-    newsItem.innerHTML = `
-    <h3>${article.title || 'No title available'}</h3>
-        <p>${article.description || 'No description available'}</p>
-        <a href="${article.url}" target="_blank">Read more</a>`;
 
-        return newsItem;
+    const image = document.createElement('img');
+    image.src = article.urlToImage ? article.urlToImage : 'path/to/default-placeholder.jpg';
+    image.alt = article.title || 'News Image';
+    image.classList.add('news-image');
+
+   
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('news-text');
+
+    const title = document.createElement('h3');
+    title.innerText = article.title;
+
+    const description = document.createElement('p');
+    description.innerText= article.description || 'No description available.';
+
+    const link = document.createElement('a');
+    link.href = article.url;
+    link.target = '_blank';
+    link.textContent = 'Read More';
+    link.classList.add('read-more');
+
+    textDiv.appendChild(title);
+    textDiv.appendChild(description);
+    textDiv.appendChild(link);
+
+    newsItem.appendChild(image);
+    newsItem.appendChild(textDiv);
+
+     return newsItem;
 }
 
 
