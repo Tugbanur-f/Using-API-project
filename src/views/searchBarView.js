7822e106d7ec6f01c2812e8552bd75c89025b145
Tugbanur <tnur.yurtsever@gmail.com>
2024-09-15 14:57:59 +0200

    a

--- src/views/searchBarView.js
+++ src/views/searchBarView.js
@@ -1,14 +1,14 @@
 
 export const createSearchBar = (onSearch) => {
-    const searchDiv = document.createElement('div');
-    searchDiv.classList.add('search-bar');
+    const searchBar = document.createElement('div');
+    searchBar.classList.add('search-bar');
 
-    searchDiv.innerHTML =`
+    searchBar.innerHTML =`
      <input type="text" id="searchInput" class="search-input" placeholder="Search news..." />
      <button id="searchButton" class="search-button">Search</button>`;
 
-     const input = searchDiv.querySelector('#searchInput');
-     const button = searchDiv.querySelector('#searchButton');
+     const input = searchBar.querySelector('#searchInput');
+     const button = searchBar.querySelector('#searchButton');
 
      button.addEventListener('click', () => onSearch(input.value));
      
@@ -18,9 +18,9 @@ export const createSearchBar = (onSearch) => {
      
      input.addEventListener('keyup', (event) => {
        if(event.key === "Enter") {
-        button.click();
+               button.click();
        } 
     });
 
-     return searchDiv;
+     return searchBar;
 }
